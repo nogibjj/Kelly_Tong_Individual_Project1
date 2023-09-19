38ee0f0d4de7acf82da951e8c276d8a0710dd358
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-18 22:23:36 -0400

    Update lib.py

--- project1/lib.py
+++ project1/lib.py
@@ -1,1 +1,20 @@
+import pandas as pd
+#import seaborn as sns
+#import matplotlib.pyplot as plt
+
+# This file includes all the functions created. 
+
+
+def view_dataset(data_path): 
+  Auto = pd.DataFrame(data_path)
+  print(Auto.info())
+  print(Auto.head())
+  print(Auto.columns())
+
+
+def describe_dataset(data_path):
+  #Auto = pd.DataFrame(data_path)
+  describe = data_path.describe()
+  markdown_describe = describe.to_markdown()
+  return markdown_output
 
