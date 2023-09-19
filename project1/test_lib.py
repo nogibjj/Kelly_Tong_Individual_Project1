2ecda72034867d8014f88c1a109b56af73d9e528
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-19 17:01:42 -0400

    Update test_lib.py

--- project1/test_lib.py
+++ project1/test_lib.py
@@ -3,4 +3,13 @@ import lib
 data = "Auto.csv"
 
 def test_scatter_mpg()：
-  scatter_mpg()
+  lib.scatter_mpg(data)
+
+def test_fitted_mpg():
+  lib.fitted_mpg(data)
+
+def test_scatter_acc():
+  lib.scatter_acc(data)
+
+def test_fitted_acc():
+  lib.fitted_acc(data)
