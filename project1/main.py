4c006083ab19dc81dbbc5bbd19c962ea8ff220be
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-18 22:26:20 -0400

    Update main.py

--- project1/main.py
+++ project1/main.py
@@ -1,1 +1,13 @@
+import lib
+import pandas
 
+def main():
+    """Main function to call data visualization functions"""
+    data = pd.read_csv("Auto.csv)
+                       
+    summary = lib.summary_statistics(data)
+    print(summary)
+
+
+if __name__ == "__main__":
+    main()
