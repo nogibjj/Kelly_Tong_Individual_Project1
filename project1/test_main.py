b5f5b136f25bffd42b063dce40ccab5e6a1947aa
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-20 18:35:38 -0400

    Update test_main.py

--- project1/test_main.py
+++ project1/test_main.py
@@ -3,3 +3,15 @@ import os
 
 def test_save_plot():
   assert os.path.exists("scatter_mpg.png")
+
+def test_save_plot2():
+  assert os.path.exists("fitted_mpg.png")
+
+def test_save_plot3():
+  assert os.path.exists("scatter_acc.png")
+
+def test_save_plot4():
+  assert os.path.exists("fitted_mpg.png")
+
+def test_save_plot5():
+  assert os.path.exists("desc_stats.md")
