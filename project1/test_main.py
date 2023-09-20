6ccb5a05d4531a179f6ecf3f7523acc870ef4d4a
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-20 14:25:56 -0400

    Update test_main.py

--- project1/test_main.py
+++ project1/test_main.py
@@ -1,6 +1,5 @@
 #Check if the images and output markdown is generated
 import os
 
-def test_plot_save(self):
-  #file_to_check = 'scatter_mpg.png'
+def test_save_plot():
   assert os.path.exists("scatter_mpg.png")
