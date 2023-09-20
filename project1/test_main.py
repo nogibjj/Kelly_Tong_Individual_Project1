1dcf7b187a403588d40b76bd986f1061b0425125
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-20 14:22:51 -0400

    Update test_main.py

--- project1/test_main.py
+++ project1/test_main.py
@@ -3,4 +3,4 @@ import os
 
 def test_plot_save(self):
   file_to_check = 'scatter_mpg.png'
-  self.assertTrue(os.path.exists("scatter_mpg.png"),f"The file '{file_to_check}' did not get created.")
+  assert os.path.exists("scatter_mpg.png")
