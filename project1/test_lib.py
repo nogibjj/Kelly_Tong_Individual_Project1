ed5202c8220270126845de01a9955d0261a795ff
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-20 01:05:40 -0400

    Update test_lib.py

--- project1/test_lib.py
+++ project1/test_lib.py
@@ -9,14 +9,14 @@ def test_describe_dataset():
 def test_scatter_mpg():
   lib.scatter_mpg(data)
 
-#def test_fitted_mpg():
-  #lib.fitted_mpg(data)
+def test_fitted_mpg():
+  lib.fitted_mpg(data)
 
-#def test_scatter_acc():
-  #lib.scatter_acc(data)
+def test_scatter_acc():
+  lib.scatter_acc(data)
 
-#def test_fitted_acc():
-  #lib.fitted_acc(data)
+def test_fitted_acc():
+  lib.fitted_acc(data)
 
-#def test_generate_general_markdown():
-  #lib.generate_general_markdown(data)
+def test_generate_general_markdown():
+  lib.generate_general_markdown(data)
